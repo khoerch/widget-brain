fd702e2935668e9a6c2005068148a097bce5236f
Kevin Hoercher <khoerch@gmail.com>
2019-08-26 18:12:25 -0400

    Set boilerplate code

---
+++ index.js
@@ -0,0 +1,17 @@
+Vue.component('widget-brain', {
+    props: {
+    },
+    data: function() {
+        return {
+        }
+    },
+    created() {
+    },
+    template: `<div>
+            <p>Hello!</p>
+        </div>`,
+});
+
+new Vue({
+    el: '#app'
+})
