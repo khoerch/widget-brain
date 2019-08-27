714623d7abdc268725fa66bfd4e5d33aac3ca012
Kevin Hoercher <khoerch@gmail.com>
2019-08-26 20:43:03 -0400

    Insert generic chart for testing

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-// var ctx = document.getElementById('myChart').getContext('2d');
-// var chart = new Chart(ctx, {
-//     // The type of chart we want to create
-//     type: 'line',
-
-//     // The data for our dataset
-//     data: {
-//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//         datasets: [{
-//             label: 'My First dataset',
-//             backgroundColor: 'rgb(255, 99, 132)',
-//             borderColor: 'rgb(255, 99, 132)',
-//             data: [0, 10, 5, 2, 20, 30, 45]
-//         }]
-//     },
-
-//     // Configuration options go here
-//     options: {}
-// });
-
 Vue.component('torque-chart', {
     props: {
-        message: {
-            type: String
-        }
     },
     data: function() {
         return {
@@ -31,7 +8,9 @@ Vue.component('torque-chart', {
     },
     created() {
         fetch('http://wb-predictivemaintenance-api.prsp7vkew2.eu-central-1.elasticbeanstalk.com/api/TorqueValues')
-            .then(response => console.log(response))
+            .then(function(response) {
+                console.log(response)
+            })
     },
     template: `<div>
             <canvas id="myChart"></canvas>
@@ -41,3 +20,23 @@ Vue.component('torque-chart', {
 new Vue({
     el: '#app',
 })
+
+const ctx = document.getElementById('myChart');
+const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
