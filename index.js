a237448af71090b218505e24694b0ad9415b9255
Kevin Hoercher <khoerch@gmail.com>
2019-08-26 20:29:07 -0400

    Add rough styling for design

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const WidgetBrain = {
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var chart = new Chart(ctx, {
+//     // The type of chart we want to create
+//     type: 'line',
+
+//     // The data for our dataset
+//     data: {
+//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//         datasets: [{
+//             label: 'My First dataset',
+//             backgroundColor: 'rgb(255, 99, 132)',
+//             borderColor: 'rgb(255, 99, 132)',
+//             data: [0, 10, 5, 2, 20, 30, 45]
+//         }]
+//     },
+
+//     // Configuration options go here
+//     options: {}
+// });
+
+Vue.component('torque-chart', {
     props: {
         message: {
             type: String
@@ -6,18 +26,18 @@ const WidgetBrain = {
     },
     data: function() {
         return {
+            data: {}
         }
     },
     created() {
+        fetch('http://wb-predictivemaintenance-api.prsp7vkew2.eu-central-1.elasticbeanstalk.com/api/TorqueValues')
+            .then(response => console.log(response))
     },
     template: `<div>
-            <p>My message: {{ message }}</p>
+            <canvas id="myChart"></canvas>
         </div>`,
-};
+});
 
-const vm = new Vue({
+new Vue({
     el: '#app',
-    components: {
-        'widget-brain': WidgetBrain,
-    }
 })
